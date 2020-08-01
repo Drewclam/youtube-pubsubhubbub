8710b60c66e6bd247248af2c2e75421c0af3e516
Andrew Lam <drewclam@hotmail.com>
2020-08-01 13:57:24 -0400

    Add query data parsing

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const http = require("http");
+const url = require("url");
 
 const listener = (req, res) => {
   console.log("Request: ", req);
+  const queryData = url.parse(req.url, true).query;
+  console.log(queryData);
   res.writeHead(200);
+  if (queryData["hub.challenge"]) {
+    res.end(queryData["hub.challenge"]);
+  }
+
   res.end("Hello world");
 };
 
