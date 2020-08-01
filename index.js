f9bd19df5c14a46501f32f1f533c177f81265df8
Andrew Lam <drewclam@hotmail.com>
2020-08-01 14:23:32 -0400

    Modify logging

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const http = require("http");
 const url = require("url");
 
 const listener = (req, res) => {
-  console.log("Request: ", req);
+  console.log("Parsing request...");
   const queryData = url.parse(req.url, true).query;
-  console.log(queryData);
+  console.log("hub challenge:", queryData["hub.challenge"]);
   res.writeHead(200);
   if (queryData["hub.challenge"]) {
     res.write(queryData["hub.challenge"]);
@@ -15,3 +15,5 @@ const listener = (req, res) => {
 
 const server = http.createServer(listener);
 server.listen(process.env.PORT || 8080);
+
+// curl -X POST https://pubsubhubbub.appspot.com/ -d'hub.mode=subscribe' -d'hub.topic=https://www.youtube.com/channel/UCu1UXd-4-X9aAhXgajYSWYw' -d'hub.callback=https://youtube-pubsubhubbub.herokuapp.com/' -d'hub.verify=sync'
